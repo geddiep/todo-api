e8361b432fffd01d16b32586fcc117284870eda5
Patrick Geddie <geddiep@gmail.com>
2016-08-30 22:39:37 -0400

    add put todos

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var PORT 	= process.env.PORT || 3000;
 var _ = require('underscore');
 
 
+
 // var todos	= [{
 // 	id: 		1,
 // 	description:'Meet mom for lunch',
@@ -18,6 +19,8 @@ var _ = require('underscore');
 // 	description: 'Walk the dog',
 // 	completed: true
 // }];
+
+
 var todos = [];
 var todoNextId = 1;
 
@@ -27,6 +30,7 @@ app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
 
+
 // GET /todos
 app.get('/todos', function(req,res){
 	res.json(todos);
@@ -34,7 +38,7 @@ app.get('/todos', function(req,res){
 
 // GET /todos/:id
 app.get('/todos/:id', function(req,res){
-	var todoId 	= parseInt(req.params.id);
+	var todoId 	= parseInt(req.params.id, 10);
 	var todoFound = _.findWhere(todos, {id: todoId});
 
 	if(todoFound != undefined){
@@ -66,7 +70,7 @@ app.post('/todos', function(req,res){
 
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req,res){
-	var todoId 	= parseInt(req.params.id);
+	var todoId 	= parseInt(req.params.id, 10);
 	var todoFound = _.findWhere(todos, {id: todoId});
 
 	if(todoFound != undefined){
@@ -77,12 +81,41 @@ app.delete('/todos/:id', function(req,res){
 	}
 });
 
+//update
+app.put('/todos/:id', function (req,res) {
 
+	//#### VALIDATION ################
+	var todoId 	= parseInt(req.params.id, 10);
+	var todoFound = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body,'description','completed'); //keep only these 2
+	var validAttributes = [];
 
+	if(!todoFound){
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	todoFound = _.extend(todoFound, validAttributes);
+	res.json(todoFound);
+});
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + '!');
 });
+
+
+
 
 
 
