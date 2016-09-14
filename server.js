b84de507afc5d39cab8ab605a80cd0e1150a48d6
Patrick Geddie <geddiep@gmail.com>
2016-09-13 21:34:18 -0400

    convert delete todos to sequelize

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.get('/', function(req, res) {
 	res.send('Todo API Root');
 });
 
-
 // GET /todos
 app.get('/todos', function(req, res) {
 	var query = req.query;
@@ -54,32 +53,6 @@ app.get('/todos', function(req, res) {
 	}, function(e){
 		res.status(500).send();
 	});
-
-
-	// //############## INITIAL try ##################
-	// var queryParams = req.query;
-	// var filteredTodos = todos;
-
-	// //completed?
-	// if (queryParams.hasOwnProperty('completed')) {
-	// 	if (queryParams.completed === 'true')
-	// 		filteredTodos = _.where(filteredTodos, {
-	// 			completed: true
-	// 		});
-	// 	else if (queryParams.completed === 'false')
-	// 		filteredTodos = _.where(filteredTodos, {
-	// 			completed: false
-	// 		});
-	// }
-	// //search
-	// if (queryParams.hasOwnProperty('search') && queryParams.search.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		if (todo.description.toLowerCase().indexOf(queryParams.search.toLowerCase()) > -1)
-	// 			return todo;
-	// 	});
-	// }
-
-	// res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -94,16 +67,6 @@ app.get('/todos/:id', function(req, res) {
 	}, function(e){
 		res.status(500).send();
 	});
-
-	// var todoFound = _.findWhere(todos, {
-	// 	id: todoId
-	// });
-
-	// if (todoFound != undefined) {
-	// 	res.json(todoFound);
-	// } else {
-	// 	res.status(404).send();
-	// }
 });
 
 //POST todos
@@ -117,47 +80,25 @@ app.post('/todos', function(req, res) {
 	}, function(e){
 		res.status(400).json(e);
 	});
-
-	// //my way
-	// db.todo.create({
-	// 	description: body.description,
-	// 	completed: body.completed
-	// }).then(function(todo){
-	// 	return res.json(todo);
-	// }).catch(function(e){
-	// 	return res.status(400).json(e);
-	// });
-
-
-	// //#### VALIDATION ################
-	// var body = _.pick(req.body, 'description', 'completed'); //keep only these 2
-	// if (!_.isBoolean(body.completed) ||
-	// 	!_.isString(body.description) ||
-	// 	body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim(); //clean it up
-	// body.id = todoNextId; //set the id
-	// todos.push(body);
-	// todoNextId++;
-	// res.json(body);
 });
 
 
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var todoFound = _.findWhere(todos, {
-		id: todoId
+	var where = {};
+	where.id = todoId;
+	db.todo.destroy({where:where}).then (function (rowsDeleted){
+		if(rowsDeleted === 0){
+			res.status(400).send({
+				error: 'No todo with id ' + todoId
+			});
+		} else{
+			res.status(204).send();
+		}
+	}, function(e){
+		res.status(500).send();
 	});
-
-	if (todoFound != undefined) {
-		todos = _.without(todos, todoFound);
-		res.json(todoFound);
-	} else {
-		res.status(404).send();
-	}
 });
 
 //update
